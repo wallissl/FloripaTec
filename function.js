14c1f59baf114cf9b2cddc97495effaffd4b7e50
Wallis Silva <wallis.sl@outlook.com>
2023-02-06 22:09:15 -0300

    Aqui começa com while

--- function.js
+++ function.js
@@ -1,5 +1,20 @@
 console.log("Hello");
 
-for( i=0;i<10;i++){
+/* for( i=0;i<11;i++){
     console.log("World");
+} */
+
+/* var alunos = [
+    'Wallis', 'Ana', 'Kami'
+];
+
+alunos.forEach((aluno) => {
+    console.log(aluno)
+}); */
+
+var contador = 0;
+
+while(contador <=5){
+    console.log('ola');
+    contador ++;
 }
